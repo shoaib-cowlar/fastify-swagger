7f06bbbcc0db5c11c749414292a8c8762779923b
shoaib naseer <shoaib.naseer.cowlar@gmail.com>
2023-06-08 12:25:39 +0500

    Added Controller and configured it in routes

---
+++ controllers/itemsController.js
@@ -0,0 +1,41 @@
+const items = require("../items");
+const { v4: uuidv4 } = require("uuid");
+
+const getItems = (req, reply) => {
+  reply.send({ items });
+};
+
+const getSingleItem = (req, reply) => {
+  try {
+    const id = req.params.id;
+    let item = items.find((item) => item.id === id);
+    if (item) {
+      reply.send({ item });
+    } else {
+      reply.status(400).send("Item Not Found");
+    }
+  } catch (error) {
+    reply.status(500).send(error);
+  }
+};
+
+const saveSingleItem = (req, reply) => {
+  try {
+    const { name } = req.body;
+    // Generate a unique ID using UUIDv4
+    const id = uuidv4();
+    const newItem = {
+      id,
+      name,
+    };
+
+    items.push(newItem);
+    reply.status(201).send({ item: newItem });
+  } catch (error) {
+    reply.status(500).send(error);
+  }
+};
+
+module.exports = {
+    getItems,getSingleItem,saveSingleItem
+}
--- routes/itemsRoutes.js
+++ routes/itemsRoutes.js
@@ -1,6 +1,4 @@
-const items = require("../items");
-const { v4: uuidv4 } = require("uuid");
-
+const {getItems,getSingleItem,saveSingleItem} = require('../controllers/itemsController')
 const Item = {
   type: "object",
   properties: {
@@ -79,40 +77,11 @@ const saveItemOpts = {
 
 function itemRoutes(fastify, options, done) {
   // Define all your routes here
-  fastify.get("/items", getItemsOpts, (req, reply) => {
-    reply.send({ items });
-  });
+  fastify.get("/items", getItemsOpts,getItems );
 
-  fastify.get("/items/:id", getItemOpts, (req, reply) => {
-    try {
-      const id = req.params.id;
-      let item = items.find((item) => item.id === id);
-      if (item) {
-        reply.send({ item });
-      } else {
-        reply.status(400).send("Item Not Found");
-      }
-    } catch (error) {
-      reply.status(500).send(error);
-    }
-  });
+  fastify.get("/items/:id", getItemOpts,getSingleItem );
 
-  fastify.post("/items",saveItemOpts, (req, reply) => {
-    try {
-      const { name } = req.body;
-      // Generate a unique ID using UUIDv4
-      const id = uuidv4();
-      const newItem = {
-        id,
-        name,
-      };
-
-      items.push(newItem);
-      reply.status(201).send({ item: newItem });
-    } catch (error) {
-      reply.status(500).send(error);
-    }
-  });
+  fastify.post("/items",saveItemOpts, saveSingleItem);
 
   done();
 }
